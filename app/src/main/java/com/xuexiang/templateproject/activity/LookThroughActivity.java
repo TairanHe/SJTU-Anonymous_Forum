ca8cbf06c628b671db66250c9d5c485dc77cbf35
TairanHe <1065593158@qq.com>
2020-09-24 23:00:04 +0800

    bug fixed

--- app/src/main/java/com/xuexiang/templateproject/activity/LookThroughActivity.java
+++ app/src/main/java/com/xuexiang/templateproject/activity/LookThroughActivity.java
@@ -75,7 +75,7 @@ public class LookThroughActivity extends AppCompatActivity implements View.OnCli
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 //        进入新帖子，重设LastSeenFloorID
-        ExchangeInfosWithAli.LastSeenThreadID = "NULL";
+        ExchangeInfosWithAli.LastSeenFloorID = "NULL";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_look_through);
 
