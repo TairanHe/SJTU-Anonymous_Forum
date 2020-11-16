7378370d93a42a33510174b275428e7cec5e4afb
TairanHe <1065593158@qq.com>
2020-11-16 11:49:32 +0800

    Added email waiting toast

--- app/src/main/java/com/xuexiang/templateproject/fragment/LoginFragment.java
+++ app/src/main/java/com/xuexiang/templateproject/fragment/LoginFragment.java
@@ -111,7 +111,7 @@ public class LoginFragment extends BaseFragment {
                     Log.d("LoginFragment.java", etPhoneNumber.getEditValue() + "   " + etVerifyCode.getEditValue());
                 break;
             case R.id.btn_register:
-                XToastUtils.info("发送验证码");
+                XToastUtils.info("验证码需要约30秒送至您的邮箱，请耐心等待～");
                 try {
                     int VarifiedEmailAddress = ExchangeInfosWithAli.Request_verifycode(etPhoneNumber.getEditValue());
                     if (VarifiedEmailAddress == 0){
